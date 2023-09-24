220e853ff766ba2e56e467cc82cf39f5dca6865e
NovaVoidHowl <me@novavoidhowl.com>
2023-09-24 22:50:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# NavMesh Follower Setup
+
+## Script Overview
+
+TO BE ADDED
+
+## How to install this package
+
+1> In unity open the `Package Manager` window
+2> Click the plus button in the top left of that window and choose the `Add packages from git URL` option
+![image](https://github.com/NovaVoidHowl/Mesh-Bone-Rebind/assets/31048789/66eaec96-322e-46ac-811d-353f8209198c)
+3> Paste in the git url of this repo `https://github.com/NovaVoidHowl/NavMesh-Follower-Setup.git`
+![image](https://github.com/NovaVoidHowl/Mesh-Bone-Rebind/assets/31048789/de07970b-7649-4789-aa7b-fc2a00622551)
+4> Click the add button
+
+The script should then be ready to use.
+
+## How to use
+
+TO BE ADDED
---
+++ Runtime/NavMeshFollowerSetup.cs
@@ -0,0 +1,31 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace uk.novavoidhowl.dev.navmeshfollowersetup
+{
+  [ExecuteInEditMode]
+  public class NavMeshFollowerSetup : MonoBehaviour
+  {
+    public GameObject navMeshFollowerBody;
+    public Transform headBone;
+    public int followerLevel = 0;
+
+    // level 0 = un-configured
+    // level 1 = simple follower
+    // level 2 = follower with LookAt IK
+    // level 3 = follower with LookAt IK and VRIK for arms
+
+
+    public float agent_speed = 3f;
+    public float agent_angularSpeed = 240f;
+    public float agent_acceleration = 10f;
+    public float agent_stoppingDistance = 1.5f;
+    public float agent_radius = 0.15f;
+    public float agent_height = 0.3f;
+
+    void Update() { }
+  }
+}
+#endif
